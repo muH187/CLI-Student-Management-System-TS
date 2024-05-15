e0b7101fa50cdfdb896e3906b286140464ef6cfa
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-15 15:34:35 +0500

    in progress

--- index.ts
+++ index.ts
@@ -48,15 +48,93 @@ class Student_manager{
         console.log(`Student: ${name} added successfully. Student ID: ${student.id}`)
     }
 
+    findStudent(student_id: number) {
+        return this.student.find(std => std.id === student_id)
+    }
+
     enroll_student(student_id: number, course: string) {
-        let student = this.student.find(std => std.id === student_id)
+        let student = this.findStudent(student_id)
         if(student) {
             student.enroll_course(course)
             console.log(`${student.name} enrolled in ${course} successfully`)
         }
     }
 
-    view_student_balance() {
+    view_student_balance(student_id: number) {
+        let student = this.findStudent(student_id)
+        if(student) {
+            student.view_balance()
+        } else {
+            console.log("Student Not Found! Please Enter A Correct Student ID.")
+        }
+    }
+
+    payStudentFee(student_id: number, amount: number) {
+        let student = this.findStudent(student_id)
+        if(student) {
+            student.pay_fees(amount)
+        } else {
+            console.log("Student Not Found! Please Enter A Correct Student ID.")
+        }
+    }
+
+    showStudentStatus(student_id: number) {
+        let student = this.findStudent(student_id)
+        if(student) {
+            student.all_details()
+        }
+    }
+}
+
+async function main() {
+    console.log("Welcome To Student Management System!")
+    console.log("-".repeat(50))
+
+    let studentManager = new Student_manager()
+
+    while(true) {
+        let choice = await inquirer.prompt([
+            {
+                name: "choice",
+                message: "Select an Option",
+                type: "list",
+                choices: [
+                    "Add Student",
+                    "Enroll Student",
+                    "View Student Balance",
+                    "Pay Fees",
+                    "Show Status",
+                    "Exit"
+                ]
+            }
+        ])
         
+        switch(choice.choice) {
+            case "Add Student":
+                let nameInput = await inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "Enter a Student name"
+                    }
+                ])
+                studentManager.add_student(nameInput.name)
+                break
+            case "Enroll Student":
+                let courseInput = await inquirer.prompt([
+                    {
+                        name: "studentId",
+                        message: "Enter Student ID",
+                        type: "number"
+                    },
+                    {
+                        name: "course",
+                        message: "Enter Course Name",
+                        type: "input"
+                    }
+                ])
+                studentManager.enroll_student(courseInput.studentId, courseInput.course)
+                break
+        }
     }
 }
